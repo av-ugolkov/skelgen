b0d357e41079f67c9f094e8c9b4d494e47fec9c3
aleksandr <ugolkov.prog@gmail.com>
2025-04-24 17:44:08 +0300

    exec commands

--- internal/generator.go
+++ internal/generator.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/av-ugolkov/gopkg/logger"
 	"gopkg.in/yaml.v3"
@@ -28,23 +29,38 @@ func startGenerate(conf map[string]any, rootPath string) error {
 	var listErrors error
 
 	for k, v := range conf {
-		switch v.(type) {
-		case map[string]any:
-			err := createFolder(rootPath, k)
-			if err != nil {
-				listErrors = errors.Join(listErrors, err)
+		switch k {
+		case string(Dirs):
+			logger.Infof("create dirs: not implemented yet")
+		case string(Files):
+			logger.Infof("create files: not implemented yet")
+		case string(Exec):
+			commands := v.([]any)
+			for _, command := range commands {
+				cmd := strings.Split(command.(string), " ")
+				runInDir(rootPath, cmd[0], cmd[1:]...)
 			}
-			err = startGenerate(v.(map[string]any), path.Join(rootPath, k))
-			if err != nil {
-				listErrors = errors.Join(listErrors, err)
-			}
-		case string:
-			err := createFile(rootPath, k, v.(string))
-			if err != nil {
-				listErrors = errors.Join(listErrors, err)
-			}
+		case string(Link):
+			logger.Infof("link file: not implemented yet")
 		default:
-			logger.Warnf("unknow command - [%s]: %v", k, v)
+			switch v.(type) {
+			case map[string]any:
+				err := createFolder(rootPath, k)
+				if err != nil {
+					listErrors = errors.Join(listErrors, err)
+				}
+				err = startGenerate(v.(map[string]any), path.Join(rootPath, k))
+				if err != nil {
+					listErrors = errors.Join(listErrors, err)
+				}
+			case string:
+				err := createFile(rootPath, k, v.(string))
+				if err != nil {
+					listErrors = errors.Join(listErrors, err)
+				}
+			default:
+				logger.Warnf("unknow command - [%s]: %v", k, v)
+			}
 		}
 	}
 
